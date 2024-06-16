8a1ab6c3fae2128ff7a7153495a4db282ab11af0
airguy <airguy-one@yandex.ru>
2024-06-16 20:38:24 +0300

    Make it works

--- core/config.py
+++ core/config.py
@@ -3,15 +3,16 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    api_key: bool = Field(title='API key of exchange money rate website')
+    project_slug: str = Field()
+    api_key: str = Field(title='API key of exchange money rate website')
     exchange_rate_base_url: str = Field('https://v6.exchangerate-api.com/v6/')
 
     @property
     def exchange_rate_url(self) -> str:
-        return f"{self.exchange_rate_base_url}/{self.api_key}/latest/"
+        return f'{self.exchange_rate_base_url}/{self.api_key}/latest/'
 
     class Config:
-        env_file = ".env"
+        env_file = '.env'
 
 
 settings = Settings()
